a707db05c5479a6438f9198d26fd3f17e4b553dd
Mike Mutaki <Mkmutaki@gmail.com>
2025-07-15 17:04:11 -0700

    Updates

---
+++ assets/browser-Dzb60yJ5.js
@@ -0,0 +1,1 @@
+import{i as e}from"./vendor-Cg5LdUgf.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
